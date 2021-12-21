51d9eb1ac73e2c38f5900c811c1ee7767de13d9d
hjnodm <1170780140@qq.com>
2021-12-21 21:32:35 +0800

    基于东六A203寝室知识图谱的问答系统

---
+++ KB_query/query_main.py
@@ -0,0 +1,75 @@
+# encoding=utf-8
+
+"""
+
+@author: SimmerChan
+
+@contact: hsl7698590@gmail.com
+
+@file: query_main.py
+
+@time: 2017/12/20 15:29
+
+@desc:main函数，整合整个处理流程。
+
+"""
+from kg_demo_movie.KB_query import jena_sparql_endpoint, question2sparql
+#from KB_query import jena_sparql_endpoint, question2sparql
+import os
+
+file_path = os.path.split(os.path.realpath(__file__))[0]
+
+
+class QAInterface:
+    def __init__(self):
+        # TODO 连接Fuseki服务器。
+        self.fuseki = jena_sparql_endpoint.JenaFuseki()
+        # TODO 初始化自然语言到SPARQL查询的模块，参数是外部词典列表。
+        self.q2s = question2sparql.Question2Sparql([os.path.join(file_path, 'external_dict', 'movie_title.txt'),
+                                                    os.path.join(file_path, 'external_dict', 'person_name.txt'),
+                                                    os.path.join(file_path, 'external_dict', 'roommate_name.txt'),
+                                                    os.path.join(file_path, 'external_dict', 'proper_noun_title.txt'),
+                                                    ])
+
+    def answer(self, question: str):
+        my_query = self.q2s.get_sparql(question)
+        if my_query is not None:
+            result = self.fuseki.get_sparql_result(my_query)
+            #print(result)
+            value = self.fuseki.get_sparql_result_value(result)
+            #print(value)
+
+            # TODO 判断结果是否是布尔值，是布尔值则提问类型是"ASK"，回答“是”或者“不知道”。
+            if isinstance(value, bool):
+                if value is True:
+                    ans = "是的"
+                else:
+                    ans = "我还不知道这个问题的答案"
+            else:
+                # TODO 查询结果为空，根据OWA，回答“不知道”
+                if len(value) == 0:
+                    ans = "我还不知道这个问题的答案"
+                elif len(value) == 1:
+                    ans = value[0]
+                else:
+                    output = ''
+                    for v in value:
+                        output += v + u'、'
+                    ans = output[0:-1]
+
+        else:
+            # TODO 自然语言问题无法匹配到已有的正则模板上，回答“无法理解”
+            ans = "我不知道你表达的意思"
+
+        return ans
+
+
+if __name__ == '__main__':
+    qa_interface = QAInterface()
+    #while True:
+        #question = input(">> 请输入问题：")
+    question = '杰的家乡是那里'
+        #question = '周润发演片子'
+    ans = qa_interface.answer(question)
+    print(ans)
+    print('#' * 100)
---
+++ KB_query/question2sparql.py
@@ -0,0 +1,54 @@
+# encoding=utf-8
+
+"""
+
+@author: SimmerChan
+
+@contact: hsl7698590@gmail.com
+
+@file: question2sparql.py
+
+@time: 2017/12/20 15:29
+
+@desc: 将自然语言转为SPARQL查询语句
+
+"""
+from kg_demo_movie.KB_query import question_temp, word_tagging
+#from KB_query import question_temp, word_tagging
+
+
+class Question2Sparql:
+    def __init__(self, dict_paths):
+        self.tw = word_tagging.Tagger(dict_paths)
+        self.rules = question_temp.rules
+
+    def get_sparql(self, question):
+        """
+        进行语义解析，找到匹配的模板，返回对应的SPARQL查询语句
+        :param question:
+        :return:
+        """
+        word_objects = self.tw.get_word_objects(question)
+        for i in word_objects:
+            print(i.token, i.pos)
+        queries_dict = dict()
+
+        for rule in self.rules:
+            query, num = rule.apply(word_objects)
+            #print(rule.apply(word_objects))
+
+
+            if query is not None:
+                queries_dict[num] = query
+
+        if len(queries_dict) == 0:
+            return None
+        elif len(queries_dict) == 1:
+            return list(queries_dict.values())[0]
+        else:
+            for it in queries_dict:
+                print(queries_dict[it])
+            # TODO 匹配多个语句，以匹配关键词最多的句子作为返回结果
+            sorted_dict = sorted(queries_dict.items(), key=lambda item: item[0], reverse=True)
+            print(sorted_dict[0][1])
+            return sorted_dict[0][1]
